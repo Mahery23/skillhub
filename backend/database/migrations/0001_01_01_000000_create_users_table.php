9e66515ce3efdf5cb86a40f5a288219a9afefdd6
Steavens <steavens2010@aol.com>
2026-04-12 10:51:27 +0400

    feat(auth): add jwt backend authentication

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('mot_de_passe');
+            $table->enum('role', ['apprenant', 'formateur']);
+            $table->timestamp('date_creation')->useCurrent();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
